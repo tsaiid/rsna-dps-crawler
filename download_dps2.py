210254e50aa4b22eea6c4da93a16cf34f8e521d3
I-Ta Tsai <ittsai@gmail.com>
2017-12-07 22:44:22 +0800

    more like a module

--- download_dps2.py
+++ download_dps2.py
@@ -10,24 +10,26 @@ from urllib.request import urlretrieve
 from urllib.error import HTTPError
 import requests
 
-SLIDE_URL = 'https://dps.rsna.org/media/{0}/presentation/images/Slide{1}.png'
 
-def download_dps2(poster_id, poster_title='', options={}):
+def download_dps2(poster_id, poster_title='', options=None):
     """
-    Main function
+    The actual download function
     """
-
+    slide_url_pattern = 'https://dps.rsna.org/media/{0}/presentation/images/Slide{1}.png'
+    is_test_mode = True if isinstance(options, dict) and options.get('test') else False
+    is_debug_mode = True if isinstance(options, dict) and options.get('debug') else False
     #poster_id = 'BR100-ED-X'
-    poster_id = options.get('poster_id', '')
+    #poster_id = options.get('poster_id', '')
     #match = re.search(r'([a-zA-Z]+)(\d+).+', poster_id)
     #subspecialty = match.group(1)
     subspecialty = poster_id[:2]
 
     # check if the poster exists
-    first_slide_url = SLIDE_URL.format(poster_id, 1)
+    first_slide_url = slide_url_pattern.format(poster_id, 1)
     r = requests.get(first_slide_url)
     if r.status_code != 200:
-        print("The poster ({}) does not exist.".format(poster_id))
+        if is_debug_mode:
+            print("The poster ({}) does not exist.".format(poster_id))
         return r.status_code
 
     # mkdirs
@@ -40,28 +42,36 @@ def download_dps2(poster_id, poster_title='', options={}):
     slide_no = 0
     while not reach_slides_end:
         slide_no += 1
-        if args.test and slide_no > 3:
+        if is_test_mode and slide_no > 3:
             break
 
-        slide_url = SLIDE_URL.format(poster_id, slide_no)
+        slide_url = slide_url_pattern.format(poster_id, slide_no)
         local_slide_path = os.path.join(downloaded_poster_path, "Slide{0}.png".format(slide_no))
         try:
             urlretrieve(slide_url, local_slide_path)
         except HTTPError as e:
             if e.code == 404:
-                print('end of poster {0} at slide {1}'.format(poster_id, slide_no))
-                print(e.reason)
+                if is_debug_mode:
+                    print('Poster {0} ends at slide {1}'.format(poster_id, slide_no))
             else:
-                print('error: {0} {1}'.format(e.code, e.reason))
+                if is_debug_mode:
+                    print('Error: {0} {1}'.format(e.code, e.reason))
             reach_slides_end = True
-    return False
+    return 0
 
-if __name__ == '__main__':
+def main():
+    """
+    Main function
+    """
     # parse poster id
     parser = argparse.ArgumentParser()
     parser.add_argument('poster_id')
     parser.add_argument('poster_title', nargs='?')
     parser.add_argument("-t", "--test", help="run in test mode; download only 3 slides", action="store_true")
+    parser.add_argument("-D", "--debug", help="run in debug mode", action="store_true")
     args = parser.parse_args()
 
     download_dps2(args.poster_id, args.poster_title, vars(args))
+
+if __name__ == '__main__':
+    main()
