778073b9ec3eb7985fff080cbe9ce1904cccae7b
I-Ta Tsai@MBPR <ittsai@gmail.com>
2017-12-08 09:49:21 +0800

    using logging package

--- bulk_download.py
+++ bulk_download.py
@@ -3,6 +3,7 @@ Bulk download RSNA DPS Poster by given CSV file and subspecialities.
 """
 
 import argparse
+import logging
 import csv
 import download_dps2
 
@@ -14,8 +15,12 @@ def main():
     parser.add_argument('csv_file')
     parser.add_argument('categories', nargs='*')
     parser.add_argument("-t", "--test", help="run in test mode; download only 3 slides", action="store_true")
-    parser.add_argument("-D", "--debug", help="run in debug mode", action="store_true")
+    parser.add_argument("-l", "--log", dest="logLevel",
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+                        default='WARNING',
+                        help="Set the logging level")
     args = parser.parse_args()
+    logging.basicConfig(level=logging.getLevelName(args.logLevel))
 
     #print(args.categories)
     csv_file = args.csv_file
@@ -24,10 +29,13 @@ def main():
     # read csv file
     with open(csv_file, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
+        if args.test:
+            print('In TEST mode, only 3 slides per poster will be downloaded.')
         for row in reader:
             #print(row['poster_id'], row['title'])
             poster_cat = row['poster_id'][:2]
             if not categories or poster_cat in categories:
+                print('Start downloading: ', row['poster_id'], row['title'])
                 download_dps2.download_dps2(row['poster_id'], row['title'], vars(args))
 
 if __name__ == '__main__':
